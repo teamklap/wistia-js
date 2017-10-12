e959056df82b40ee214c3ba29f38bc250719ae36
m-h-t <git@integer.store>
2017-10-12 16:08:50 +0200

    Beautify

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
+"use strict";
 /*
  * @module
  * @description
  * Main entry point for all Wistia APIs
  */
-
-"use strict";
-
 module.exports = function (apiKey, opts) {
-
     if (!apiKey) {
-
         throw new Exception('No key provided!');
-
     }
 
     opts = opts || {};
 
     var options = {
-
         version: opts.version || 'v1',
         format: opts.format || 'json'
-
     };
 
     return {
