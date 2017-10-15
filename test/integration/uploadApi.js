81bc8ff954e2d2ff5da3ed36c366962cf53fab07
m-h-t <git@integer.store>
2017-10-15 11:46:35 +0200

    Adapt uploadApi integration test

--- test/integration/uploadApi.js
+++ test/integration/uploadApi.js
@@ -2,15 +2,29 @@ const fs = require('fs');
 const test = require('ava');
 
 const config = require('./config.json');
-const Wistia = require('../../')(config.apiKey);
+const wistia = require('../../')(config.apiKey);
 
-const WistiaUpload = Wistia.WistiaUpload();
+const uploadApi = wistia.uploadApi();
+const dataApi = wistia.dataApi();
+
+let testProject;
+
+test.before(async t => {
+	testProject = await dataApi.projects.create({
+		name: 'Wistiajs Integration Upload Api Test Project',
+		description: 'This project was created as part of the wistiajs integration test. It can be deleted'
+	});
+})
+
+test.after(async t => {
+	await dataApi.projects.delete(testProject.hashedId);
+});
 
 test('Upload video to Wistia', async t => {
-	const videoMetadata = await WistiaUpload.upload({
+	const videoMetadata = await uploadApi.upload({
 		name: 'Test Video',
+		project_id: testProject.id,
 		description: 'This video was uploaded for integration testing',
-		project_id: config.projectId,
 		file: fs.createReadStream(config.sampleVideoPath)
 	});
 
