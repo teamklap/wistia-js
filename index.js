cb260641a2e9732d0a9554107080af1528367ac5
m-h-t <git@integer.store>
2017-10-25 09:20:28 +0200

    index: apiKey to apiPassword, wistia wording

--- index.js
+++ index.js
@@ -9,22 +9,21 @@ const debug = require('debug-levels')('wistiajs:index');
  * @description
  * Main entry point for all Wistia APIs
  */
-module.exports = function (apiKey, {apiVersion = 'v1', responseFormat = 'json'} = {}) {
-    if (!apiKey) {
-        throw new Exception('No key provided!');
+module.exports = function (apiPassword, {apiVersion = 'v1', responseFormat = 'json'} = {}) {
+    if (!apiPassword) {
+        throw new Error('No API password provided!');
     }
 
     debug.verbose(`Wistiajs: api version: ${apiVersion}, response format: ${responseFormat}`);
 
     return {
-        //Data API ==> https://wistia.com/doc/data-api
+        // Data API ==> https://wistia.com/doc/data-api
         dataApi: function () {
-            return new DataApi(apiKey, {apiName: 'data', apiVersion, responseFormat})
+            return new DataApi(apiPassword, {apiName: 'data', apiVersion, responseFormat})
         },
-        //Upload API ==> https://wistia.com/doc/upload-api
+        // Upload API ==> https://wistia.com/doc/upload-api
         uploadApi: function () {
-            return new UploadApi(apiKey, {apiName: 'upload', apiVersion, responseFormat})
+            return new UploadApi(apiPassword, {apiName: 'upload', apiVersion, responseFormat})
         }
     }
-
 }
